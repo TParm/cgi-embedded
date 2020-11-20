c115740634824333d5ce305a800be951eef6dc00
AlexanderT <38465150+AlexaTack@users.noreply.github.com>
2020-11-20 11:32:59 +0100

    Added example fetch

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,19 @@
-console.log('script')
+$(() => {
+    fetchData('https://randomuser.me/api/')
+        .then((x) => {
+            console.log(x)
+            const td = d => `<td>${d}</td>`;
+            const gender = td(x.results[0].gender);
+            const tr = `<tr>${gender}</tr>`
+            $('#tbody-data').append(tr);
+        })
+})
+
+const fetchData = async (url) => {
+    const response = await fetch(url);
+    if (!response.ok) {
+        alert(`Something went wrong, status ${response.status}.`);
+        return;
+    }
+    return await response.json();
+}
