7683a35a392b87137c27554e71cb791c0973e1e8
root <root@bopi.mshome.net>
2020-11-23 22:00:55 +0100

    Write to text file success

--- CGI/source/main.c
+++ CGI/source/main.c
@@ -1,20 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define DATAFILE "/var/www/html/cgi-embedded/data/cgidata.json"
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define MAXLEN 80
+#define MAXINPUT MAXLEN+2
+#define DATAFILE "/var/www/html/cgi-embedded/data/cgidata.txt" // File path
+
+void unencode(char *src, char *last, char *dest)
+{
+ for(; src != last; src++, dest++)
+   if(*src == '+')
+     *dest = ' ';
+   else if(*src == '%') {
+     int code;
+     if(sscanf(src+1, "%2x", &code) != 1) code = '?';
+     *dest = code;
+     src +=2; }
+   else
+     *dest = *src;
+ *dest = '\n';
+ *++dest = '\0';
+}
+
+/* Print a basic HTTP header. */
+
+static void print_http_header(const char *content_type)
+{
+    printf("Content-Type: %s\n\n", content_type);
+}
+
 int main(void)
 {
-FILE *f = fopen(DATAFILE,"r");
-int ch;
-if(f == NULL) {
-  printf("%s%c%c\n",
-  "Content-Type:text/html;charset=iso-8859-1",13,10);
-  printf("<TITLE>Failure</TITLE>\n");
-  printf("<P><EM>Unable to open data file, sorry!</EM>"); }
+char *lenstr;
+char input[MAXINPUT], data[MAXINPUT];
+long len;
+
+
+print_http_header("text/html");
+printf("<TITLE>Response</TITLE>\n");
+lenstr = getenv("CONTENT_LENGTH");
+if(lenstr == NULL || sscanf(lenstr,"%ld",&len)!=1 || len > MAXLEN)
+  printf("<P>Error in invocation - wrong FORM probably.");
 else {
-  printf("%s%c%c\n",
-  "Content-Type:text/plain;charset=iso-8859-1",13,10);
-  while((ch=getc(f)) != EOF)
-    putchar(ch);
-  fclose(f); }
+  FILE *f;
+  fgets(input, len+1, stdin);
+  unencode(input, input+len, data);
+  f = fopen(DATAFILE, "a");
+  if(f == NULL)
+    printf("<P>Sorry, cannot store your data.");
+  else
+    fputs(data, f);
+  fclose(f);
+  printf("<P>Thank you! Your contribution has been stored.");
+  }
 return 0;
 }
